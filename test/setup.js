30fbbcf17e390df28c68de91d4efe51d4712e520
Fredrik Löwenhamn <fredrik.lowenhamn@ica.se>
2022-07-11 18:21:43 +0200

    Wip

--- test/setup.js
+++ test/setup.js
@@ -2,12 +2,7 @@ const baseSetup = require('jest-environment-puppeteer/lib/global').setup
 const util = require('util')
 const exec = util.promisify(require('child_process').exec)
 
-const ts = 'yarn brine'
-const js = 'yarn build && node ./dist/cli.js'
-
-const builder = ts
-
 module.exports = async (args) => {
-  await exec(`${builder} build -x ex -o ./test/dist ./test/example`)
+  await exec('yarn build:example')
   await baseSetup(args)
 }
