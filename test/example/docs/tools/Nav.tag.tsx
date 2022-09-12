c98839253a6978eaaa5034612e302069d94361e7
Fredrik Löwenhamn <fredrik.lowenhamn@ica.se>
2022-09-12 11:59:35 +0200

    Adding example components

--- test/example/docs/tools/Nav.tag.tsx
+++ test/example/docs/tools/Nav.tag.tsx
@@ -1,13 +1,35 @@
 export const DocsNav = ({ links }: { links: string[] }) => {
+  const groupedLinks = links.reduce((acc, cur) => {
+    const parts = cur.split('--')
+    const category = parts.length === 2 ? parts[0] : ''
+    const root = parts.length === 1
+    const name = parts.length === 2 ? parts[1] : parts[0]
+    if (!acc[category]) acc[category] = []
+    acc[category].push({ name, link: `#${cur}`, root })
+    return acc
+  }, {} as { [i: string]: { name: string; link: string; root: boolean }[] })
   return (
     <>
-      {links
-        .sort((a, b) => (a.localeCompare(b) ? 1 : -1))
-        .map((prop, index) => (
-          <a key={index} href={`#${prop}`}>
-            {prop}
-          </a>
-        ))}
+      {Object.entries(groupedLinks).map(([groupKey, groupValue]) => (
+        <div
+          key={groupKey}
+          style={{ display: 'flex', flexDirection: 'column' }}
+        >
+          <div>{groupKey}</div>
+          {groupValue.map((group) => (
+            <a
+              key={group.name}
+              href={group.link}
+              style={{
+                marginLeft: group.root ? '' : '0.4rem',
+                marginBottom: '3px',
+              }}
+            >
+              {group.name.replace('_', '')}
+            </a>
+          ))}
+        </div>
+      ))}
     </>
   )
 }
