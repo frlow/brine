b8e599c2fcb95d1b5d2a60f149c57163221effeb
Fredrik Löwenhamn <fredrik.lowenhamn@ica.se>
2022-12-04 20:28:47 +0100

    Wip

--- src/build/analysis/common.test.ts
+++ src/build/analysis/common.test.ts
@@ -8,78 +8,165 @@ export type AnalyzerTestCases = {
   literalProp: string
   optionalProp: string
   multipleProps: string
-  camelName: string
-  kebabName: string
-  exoticName: string
+  camelNameProp: string
+  kebabNameProp: string
+  exoticNameProp: string
+  stringEmit: string
+  numberEmit: string
+  objectEmit: string
+  literalEmit: string
+  optionalEmit: string
+  multipleEmits: string
+  camelNameEmit: string
+  kebabNameEmit: string
+  exoticNameEmit: string
+  noSlots: string
+  defaultSlot: string
+  namedSlot: string
+  multipleNamedSlots: string
 }
 export const testAnalyzer = (
   analyze: AnalyzeFileFunction,
   testCases: AnalyzerTestCases
 ) => {
-  describe('FilePath', () => {
-    test('Tag name', async () => {
-      const result = await analyze(
-        testCases.tagName.fileName,
-        testCases.tagName.code
-      )
-      expect(result.tag).toEqual('my-app')
-    })
+  test('Name', async () => {
+    const result = await analyze(
+      testCases.tagName.fileName,
+      testCases.tagName.code
+    )
+    expect(result.tag).toEqual('my-app')
+    expect(result.name).toEqual('MyApp')
   })
   describe('Props', () => {
-    test('String Prop', async () => {
+    test('String', async () => {
       const result = await analyze('dummy.js', testCases.stringProp)
       expect(result.props).toStrictEqual([
         { name: 'str', type: 'string', optional: false },
       ])
     })
-    test('Number Prop', async () => {
+    test('Number', async () => {
       const result = await analyze('dummy.js', testCases.numberProp)
       expect(result.props).toStrictEqual([
         { name: 'num', type: 'number', optional: false },
       ])
     })
-    test('Object Prop', async () => {
+    test('Object', async () => {
       const result = await analyze('dummy.js', testCases.objectProp)
       expect(result.props).toStrictEqual([
         { name: 'obj', type: '{val:string}', optional: false },
       ])
     })
-    test('Literal Prop', async () => {
+    test('Literal', async () => {
       const result = await analyze('dummy.js', testCases.literalProp)
       expect(result.props).toStrictEqual([
         { name: 'literal', type: "'a'|'b'", optional: false },
       ])
     })
-    test('Optional Prop', async () => {
+    test('Optional', async () => {
       const result = await analyze('dummy.js', testCases.optionalProp)
       expect(result.props).toStrictEqual([
         { name: 'optional', type: 'string', optional: true },
       ])
     })
-    test('Multiple Props', async () => {
+    test('Multiple', async () => {
       const result = await analyze('dummy.js', testCases.multipleProps)
       expect(result.props).toStrictEqual([
         { name: 'a', type: 'string', optional: false },
         { name: 'b', type: 'number', optional: false },
       ])
     })
-    test('Camel name', async () => {
-      const result = await analyze('dummy.js', testCases.camelName)
+    test('Camel Name', async () => {
+      const result = await analyze('dummy.js', testCases.camelNameProp)
       expect(result.props).toStrictEqual([
         { name: 'camel-name', type: 'string', optional: false },
       ])
     })
-    test('Kebab name', async () => {
-      const result = await analyze('dummy.js', testCases.kebabName)
+    test('Kebab Name', async () => {
+      const result = await analyze('dummy.js', testCases.kebabNameProp)
       expect(result.props).toStrictEqual([
         { name: 'kebab-name', type: 'string', optional: false },
       ])
     })
-    test('Exotic name', async () => {
-      const result = await analyze('dummy.js', testCases.exoticName)
+    test('Exotic Name', async () => {
+      const result = await analyze('dummy.js', testCases.exoticNameProp)
       expect(result.props).toStrictEqual([
         { name: 'ex0t_ic', type: 'string', optional: false },
       ])
+    })
+  })
+  describe('Emits', () => {
+    test('String', async () => {
+      const result = await analyze('dummy.js', testCases.stringEmit)
+      expect(result.emits).toStrictEqual([
+        { name: 'str', type: 'string', optional: false },
+      ])
+    })
+    test('Number', async () => {
+      const result = await analyze('dummy.js', testCases.numberEmit)
+      expect(result.emits).toStrictEqual([
+        { name: 'num', type: 'number', optional: false },
+      ])
+    })
+    test('Object', async () => {
+      const result = await analyze('dummy.js', testCases.objectEmit)
+      expect(result.emits).toStrictEqual([
+        { name: 'obj', type: '{val:string}', optional: false },
+      ])
+    })
+    test('Literal', async () => {
+      const result = await analyze('dummy.js', testCases.literalEmit)
+      expect(result.emits).toStrictEqual([
+        { name: 'literal', type: "'a'|'b'", optional: false },
+      ])
+    })
+    test('Optional', async () => {
+      const result = await analyze('dummy.js', testCases.optionalEmit)
+      expect(result.emits).toStrictEqual([
+        { name: 'optional', type: 'string', optional: true },
+      ])
+    })
+    test('Multiple', async () => {
+      const result = await analyze('dummy.js', testCases.multipleEmits)
+      expect(result.emits).toStrictEqual([
+        { name: 'a', type: 'string', optional: false },
+        { name: 'b', type: 'number', optional: false },
+      ])
+    })
+    test('Camel Name', async () => {
+      const result = await analyze('dummy.js', testCases.camelNameEmit)
+      expect(result.emits).toStrictEqual([
+        { name: 'camel-name', type: 'string', optional: false },
+      ])
+    })
+    test('Kebab Name', async () => {
+      const result = await analyze('dummy.js', testCases.kebabNameEmit)
+      expect(result.emits).toStrictEqual([
+        { name: 'kebab-name', type: 'string', optional: false },
+      ])
+    })
+    test('Exotic name', async () => {
+      const result = await analyze('dummy.js', testCases.exoticNameEmit)
+      expect(result.emits).toStrictEqual([
+        { name: 'ex0t_ic', type: 'string', optional: false },
+      ])
+    })
+  })
+  describe('Slots', () => {
+    test('No Slots', async () => {
+      const result = await analyze('dummy.js', testCases.noSlots)
+      expect(result.slots).toEqual(undefined)
+    })
+    test('Default Slot', async () => {
+      const result = await analyze('dummy.js', testCases.defaultSlot)
+      expect(result.slots).toEqual([])
+    })
+    test('Named Slot', async () => {
+      const result = await analyze('dummy.js', testCases.namedSlot)
+      expect(result.slots).toEqual(['named'])
+    })
+    test('Multiple Named Slot', async () => {
+      const result = await analyze('dummy.js', testCases.multipleNamedSlots)
+      expect(result.slots).toEqual(['a', 'b'])
     })
   })
 }
--- src/build/analysis/jsx.test.ts
+++ src/build/analysis/jsx.test.ts
@@ -3,15 +3,28 @@ import { analyzeJsxFile } from './jsx.js'
 
 describe('jsx', () => {
   testAnalyzer(analyzeJsxFile, {
-    tagName: { fileName: 'myApp.tsx', code: `export default ()=>{}` },
+    tagName: { fileName: 'MyApp.tsx', code: `export default ()=>{}` },
     stringProp: `export default (args: {str: string})=>{}`,
     numberProp: `export default (args: {num: number})=>{}`,
     objectProp: `export default (args: {obj: {val:string}})=>{}`,
     literalProp: `export default (args: {literal: 'a'|'b'})=>{}`,
     optionalProp: `export default (args: {optional?: string})=>{}`,
     multipleProps: `export default (args: {a: string, b: number})=>{}`,
-    camelName: `export default (args: {camelName: string})=>{}`,
-    kebabName: `export default (args: {"kebab-name": string})=>{}`,
-    exoticName: `export default (args: {ex0t_ic: string})=>{}`,
+    camelNameProp: `export default (args: {camelName: string})=>{}`,
+    kebabNameProp: `export default (args: {"kebab-name": string})=>{}`,
+    exoticNameProp: `export default (args: {ex0t_ic: string})=>{}`,
+    stringEmit: `export default (args: {onStr: (arg: string)=>void})=>{}`,
+    numberEmit: `export default (args: {onNum: (arg: number)=>void})=>{}`,
+    objectEmit: `export default (args: {onObj: (arg: {val:string}})=>void})=>{}`,
+    literalEmit: `export default (args: {onLiteral: (arg: 'a'|'b')=>void})=>{}`,
+    optionalEmit: `export default (args: {onOptional?: (arg: string)=>void})=>{}`,
+    multipleEmits: `export default (args: {onA: (arg: string)=>void,onB: (arg: number)=>void})=>{}`,
+    camelNameEmit: `export default (args: {onCamelName: (arg: string)=>void})=>{}`,
+    kebabNameEmit: `export default (args: {"onKebab-name": (arg: string)=>void})=>{}`,
+    exoticNameEmit: `export default (args: {onEx0t_ic: (arg: string)=>void})=>{}`,
+    noSlots: `export default ()=>{return <></>}`,
+    defaultSlot: `export default ()=>{return <><slot></slot></>}`,
+    namedSlot: `export default ()=>{return <><slot name="named"></slot></>}`,
+    multipleNamedSlots: `export default ()=>{return <><slot name="a"></slot><slot name="b"></slot></>}`,
   })
 })
