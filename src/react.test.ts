8c507c4e947d18ad4246353be85ca7fe9fdc9fa4
Fredrik Löwenhamn <fredrik.lowenhamn@ica.se>
2022-12-06 21:29:17 +0100

    Wip

--- src/react.test.ts
+++ src/react.test.ts
@@ -1,11 +1,17 @@
 import { testWrapper } from './common.test.js'
 import { createOptions } from './react.js'
 
+const i = "import React from 'react'; export default "
 describe('react', () => {
   testWrapper(
     createOptions,
     {
-      stringText: `import React from 'react'; export default ()=><div>simple-string-text</div>`,
+      stringText: `${i}()=><div>simple-string-text</div>`,
+      stringProp: `${i}({text})=><div>{text}</div>`,
+      numProp: `${i}({num})=><div>{num+1}</div>`,
+      objProp: `${i}({obj})=><div>{obj.val}</div>`,
+      onMountProps: `${i}({text})=>{console.log(text);return <p/>}`,
+      simpleEvent: `${i}({onMyEvent})=><button id="button" onClick={()=>onMyEvent('simple')}/>`,
     },
     [],
     '.tsx',
