4aa895aa3a449590ac0b3275f8be2ac6a0f27f3e
Fredrik Löwenhamn <fredrik.lowenhamn@ica.se>
2022-12-12 10:54:34 +0100

    Wip

--- version.js
+++ version.js
@@ -32,6 +32,7 @@ const packageJson = {
     },
     './build': {
       import: './build/index.js',
+      require: './build/cjs/build/index.js',
       types: './build/index.d.ts',
     },
     './extensions': {
