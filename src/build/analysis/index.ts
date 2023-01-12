24564738bd3a1d6c3c913fa4238f8c82562f6fc8
Fredrik Löwenhamn <fredrik.lowenhamn@ica.se>
2023-01-12 12:49:36 +0100

    Wip

--- src/build/analysis/index.ts
+++ src/build/analysis/index.ts
@@ -2,8 +2,6 @@ import { analyzeJsxFile } from './jsx.js'
 import { analyzeVueFile } from './vue.js'
 import { analyzeSvelteFile } from './svelte.js'
 import type { Framework } from '../framework.js'
-import { Simulate } from 'react-dom/test-utils'
-import canPlay = Simulate.canPlay
 
 export { kebabize, camelize } from '../../common.js'
 export * from '../framework.js'
