8acbb85587ad38fa2d14d2ddb3983ee95502d0b2
Fredrik Löwenhamn <fredrik.lowenhamn@ica.se>
2023-03-01 14:50:24 +0100

    Wip

--- examples/prod.ts
+++ examples/prod.ts
@@ -3,8 +3,3 @@ import('./apps/svelte/SvelteApp/mysvelteapp.js')
 import('./apps/solid/SolidApp/mysolidapp.js')
 import('./apps/svelte/Tester/mytester.js')
 import('./apps/vue/VueApp/myvueapp.js')
-
-// defineAutoLoader({
-//   liteMeta: require('./apps/vue/VueApp/VueApp.lite.js').lite,
-//   loader: () => import('./apps/vue/VueApp/myvueapp.js'),
-// })
