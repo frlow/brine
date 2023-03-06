29a0c863200b0d45e50d4e0d187a506a05eb84cb
Fredrik Löwenhamn <fredrik.lowenhamn@ica.se>
2023-03-07 00:30:36 +0100

    Wip

--- src/build/analysis/appendJsx.ts
+++ src/build/analysis/appendJsx.ts
@@ -2,10 +2,12 @@ import { analyzeJsxFile } from './jsx'
 
 export const appendJsxProps = async (filePath: string, source: string) => {
   const ar = await analyzeJsxFile(filePath, source)
-  return `${source.replace(
-    'export default function (',
-    'export default function __component ('
-  )}
+  return source.includes('export default function (')
+    ? `${source.replace(
+        'export default function (',
+        'export default function __component ('
+      )}
 __component.__props = [${ar.props.map((p) => `'${p.name}'`).join(',')}]
 __component.__emits = [${ar.emits.map((e) => `'${e.name}'`).join(',')}]`
+    : source
 }
